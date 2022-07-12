ca92666786a82a5c05e1fd91120cbbb09e187fa7
yamsunsee <88229483+Yamsunsee@users.noreply.github.com>
2022-07-12 17:47:13 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/yamonus/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/yamonus.yamdev.online/" : "/",
 }
